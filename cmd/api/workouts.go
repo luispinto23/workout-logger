e1b401f093172fdf92c13af945a936c4d9a472fb
Luís Pinto <luispinto23@gmail.com>
2024-01-15 00:43:17 +0000

    refactor: add more exercise details.

--- cmd/api/workouts.go
+++ cmd/api/workouts.go
@@ -7,17 +7,26 @@ import (
 )
 
 func (app *application) createWorkoutHandler(w http.ResponseWriter, r *http.Request) {
+	type Equipment struct {
+		Name string `json:"name"`
+	}
+
 	type exercise struct {
-		Name      string `json:"name"`
-		SeriesMin int    `json:"series_min"`
-		SeriesMax int    `json:"series_max"`
-		RepMin    int    `json:"rep_min"`
-		RepMax    int    `json:"rep_max"`
-		RestMin   int    `json:"rest_min"`
-		RestMax   int    `json:"rest_max"`
-		Weight    int    `json:"weight"`
-		Time      int    `json:"time"`
-		Comment   string `json:"comment"`
+		Name        string `json:"name"`
+		MuscleGroup string `json:"muscle_group"`
+		Type        string `json:"type"`
+		Equipment   []Equipment
+		Difficulty  string `json:"difficulty"`
+		SeriesMin   int    `json:"series_min"`
+		SeriesMax   int    `json:"series_max"`
+		RepMin      int    `json:"rep_min"`
+		RepMax      int    `json:"rep_max"`
+		RestMin     int    `json:"rest_min"`
+		RestMax     int    `json:"rest_max"`
+		Weight      int    `json:"weight"`
+		Duration    int    `json:"duration"`
+		VideoURL    string `json:"video_url"`
+		Comment     string `json:"comment"`
 	}
 
 	type block struct {
